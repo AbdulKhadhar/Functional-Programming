f55d3460ad032f6066241cb9e223558b9304b891
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-07-28 12:51:40 +0530

    Avoid Mutations and Side Effects
    Avoid Mutations and Side Effects Using Functional Programming

---
+++ JavaScript/AvdMutationsSideEfcts.js
@@ -0,0 +1,10 @@
+// the global variable
+var fixedValue = 4;
+
+function incrementer() {
+  return fixedValue + 1;
+
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
