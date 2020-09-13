9363f5a0c527d2b318b92ffd16b970a401629fe8
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-09-13 14:00:03 +0530

    Split a String into an Array
    Split a String into an Array Using the split Method

---
+++ JavaScript/split.js
@@ -0,0 +1,12 @@
+var str = "Hello World";
+var bySpace = str.split(" ");
+// Sets bySpace to ["Hello", "World"]
+
+var otherString = "How9are7you2today";
+var byDigits = otherString.split(/\d/);
+// Sets byDigits to ["How", "are", "you", "today"]
+
+function splitify(str) {
+  return str.split(/\W/)
+}
+splitify("Hello World,I-am code");
